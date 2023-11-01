9c498285f22a4b6ec14571f8492fcb30e7ee7561
LENOVO <annisaurohmah@mail.ugm.ac.id>
2023-11-01 14:43:12 +0700

    fix resizing when update picture

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class PostController extends Controller
 {
@@ -57,43 +58,70 @@ class PostController extends Controller
     }
     
     
-    public function update(Request $request, $id){
+    public function update(Request $request, $id){      
+        
         if ($request->hasFile('photo')) {
-            
-        $request->validate([
-            'name' => 'required|string|max:250',
-            'email' => 'required|max:255|email:dns|unique:users,email,' . $id,
-            'photo' => 'image|nullable|mimes:jpg,png,jpeg|max:2048',
-        ]);
-        if ($request->hasFile('photo')) {
+            $request->validate([
+                'name' => 'required|string|max:250',
+                'email' => 'required|max:255|email:dns|unique:users,email,' . $id,
+                'photo' => 'image|nullable|mimes:jpg,png,jpeg|max:2048',
+            ]);
+
+            $image = $request->photo;
             $filenameWithExt = $request->file('photo')->getClientOriginalName();
             $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
             $extension = $request->file('photo')->getClientOriginalExtension();
             $filenameToStore = $filename . '_' . time() . '.' . $extension;
-            $path = $request->file('photo')->storeAs('photos', $filenameToStore);
+            $user = User::find($id);
+            $photo = $user->photo; 
+
+            // Hapus gambar asli
+            $originalPath = public_path('storage/photos/original/' . $photo);
+            if (File::exists($originalPath)) {
+                File::delete($originalPath);
+            }
+            //simpan gambar asli
+            $path = $request->file('photo')->storeAs('photos/original', $filenameToStore);
+
+
+            $thumbnailPath = public_path('storage/photos/thumbnail/' . $photo);
+            if (File::exists($thumbnailPath)) {
+                File::delete($thumbnailPath);
+            }
+            // Buat thumbnail dengan lebar dan tinggi yang diinginkan
+            $thumbnailPath = public_path('storage/photos/thumbnail/' . $filenameToStore);
+            Image::make($image)
+                ->fit(100, 100)
+                ->save($thumbnailPath);
+
+            $squarePath = public_path('storage/photos/square/' . $photo);
+            if (File::exists($squarePath)) {
+                File::delete($squarePath);
+            }
+            // Buat versi persegi dengan lebar dan tinggi yang sama
+            $squarePath = public_path('storage/photos/square/' . $filenameToStore);
+            Image::make($image)
+                ->fit(200, 200)
+                ->save($squarePath);
+
+            $path = $filenameToStore;
+            User::where('id', $id)->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'photo' => $path,
+            ]);
+
         } else {
-            $path = null;
-        }
-        User::where('id', $id)->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'photo' => $path,
-        ]);        
-
-
-    } else {
-        $request->validate([
-            'name' => 'required|string|max:250',
-            'email' => 'required|max:255|email:dns|unique:users,email,' . $id,
-        ]);
-        User::where('id', $id)->update([
-            'name' => $request->name,
-            'email' => $request->email,
-        ]);
-    }
-
+            $request->validate([
+                'name' => 'required|string|max:250',
+                'email' => 'required|max:255|email:dns|unique:users,email,' . $id,
+            ]);
+            User::where('id', $id)->update([
+                'name' => $request->name,
+                'email' => $request->email,
+            ]);
+        };
         return redirect('/users')->with('success', 'Data berhasil diupdate');
-
 
     }
 }
